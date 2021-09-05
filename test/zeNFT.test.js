49b5fa7540e50e3bc3cda700d7d05faef9baddd4
Ross Campbell <41117279+Ro5s@users.noreply.github.com>
2021-09-05 01:07:38 -0400

    Update zeNFT.test.js
    restyle test

--- test/zeNFT.test.js
+++ test/zeNFT.test.js
@@ -1,57 +1,60 @@
-const { BN, constants, expectEvent, expectRevert } = require('@openzeppelin/test-helpers');
-const { ZERO_ADDRESS } = constants;
-const { shouldSupportInterfaces } = require('../../../utils/introspection/SupportsInterface.behavior');
+const { artifacts, ethers, web3 } = require("hardhat");
+const { expect } = require("chai");
+const chai = require("chai");
+const { solidity } = require("ethereum-waffle");
 
-const { expect } = require('chai');
+chai
+  .use(solidity)
+  .should();
 
-const zeNFT = artifacts.require('zeNFT');
+const ZERO_ADDRESS = 0x0000000000000000000000000000000000000000;
 
-contract('zeNFT', function (accounts) {
-  const [ deployer, other ] = accounts;
+const name = "ZEN";
+const symbol = "ZEN";
+const baseURI = "zenft.farm/";
 
-  const name = 'ZEN';
-  const symbol = 'ZEN';
-  const baseURI = 'my.app/';
+describe("zeNFT", function () {
 
-  beforeEach(async function () {
-    this.token = await zeNFT.new(name, symbol, baseURI, { from: deployer });
+  it("NFT has correct name and symbol", async function () {
+    const NFT = await ethers.getContractFactory("zeNFT");
+    const nft = await NFT.deploy("ZEN", "ZEN", "zenft.farm/");
+    await nft.deployed();
+
+    expect(await nft.name()).to.equal("ZEN");
+    expect(await nft.symbol()).to.equal("ZEN");
   });
 
-  shouldSupportInterfaces(['ERC721', 'ERC721Enumerable']);
+  it("NFTs have correct metadata after mint", async function () {
+    let minter;
+    [minter] = await ethers.getSigners();
 
-  it('token has correct name', async function () {
-    expect(await this.token.name()).to.equal(name);
+    const NFT = await ethers.getContractFactory("zeNFT");
+    const nft = await NFT.deploy("ZEN", "ZEN", "zenft.farm/");
+    await nft.deployed();
+
+    await nft.mint();
+    expect(await nft.tokenURI(0)).to.equal("zenft.farm/0");
+    await nft.mint();
+    expect(await nft.tokenURI(1)).to.equal("zenft.farm/1");
+    await nft.mint();
+    expect(await nft.tokenURI(2)).to.equal("zenft.farm/2");
   });
 
-  it('token has correct symbol', async function () {
-    expect(await this.token.symbol()).to.equal(symbol);
-  });
+  it("NFT minters have correct balances and total supply matches", async function () {
+    let minter;
+    [minter0, minter1] = await ethers.getSigners();
 
-  describe('minting', function () {
-    it('public can mint tokens', async function () {
-      const tokenId = new BN('0');
+    const NFT = await ethers.getContractFactory("zeNFT");
+    const nft = await NFT.deploy("ZEN", "ZEN", "zenft.farm/");
+    await nft.deployed();
 
-      const receipt = await this.token.mint(other, { from: deployer });
-      expectEvent(receipt, 'Transfer', { from: ZERO_ADDRESS, to: other, tokenId });
+    await nft.mint();
+    expect(await nft.balanceOf(minter0.address)).to.equal("1");
+    await nft.mint();
+    expect(await nft.balanceOf(minter0.address)).to.equal("2");
+    await nft.connect(minter1).mint();
+    expect(await nft.balanceOf(minter1.address)).to.equal("1");
 
-      expect(await this.token.balanceOf(other)).to.be.bignumber.equal('1');
-      expect(await this.token.ownerOf(tokenId)).to.equal(other);
-
-      expect(await this.token.tokenURI(tokenId)).to.equal(baseURI + tokenId);
-    });
-
-  describe('burning', function () {
-    it('holders can burn their tokens', async function () {
-      const tokenId = new BN('0');
-
-      await this.token.mint(other, { from: deployer });
-
-      const receipt = await this.token.burn(tokenId, { from: other });
-
-      expectEvent(receipt, 'Transfer', { from: other, to: ZERO_ADDRESS, tokenId });
-
-      expect(await this.token.balanceOf(other)).to.be.bignumber.equal('0');
-      expect(await this.token.totalSupply()).to.be.bignumber.equal('0');
-    });
+    expect(await nft.totalSupply()).to.equal("3");
   });
 });
