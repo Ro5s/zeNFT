1ed3a3c9a38fd7f8907be06a44659004c4482771
Ross Campbell <41117279+Ro5s@users.noreply.github.com>
2021-09-05 22:22:52 -0400

    Update zeNFT.test.js
    level up tests to include fee and cap cases ⬆️

--- test/zeNFT.test.js
+++ test/zeNFT.test.js
@@ -10,12 +10,14 @@ chai
 const name = "ZEN";
 const symbol = "ZEN";
 const baseURI = "zenft.farm/";
+const mintCap = 5;
+const mintFee = ethers.utils.parseEther("0.5");
 
 describe("zeNFT", function () {
 
   it("NFT has correct name and symbol", async function () {
     const NFT = await ethers.getContractFactory("zeNFT");
-    const nft = await NFT.deploy(name, symbol, baseURI);
+    const nft = await NFT.deploy(name, symbol, baseURI, mintCap, 0);
     await nft.deployed();
 
     expect(await nft.name()).to.equal("ZEN");
@@ -24,22 +26,22 @@ describe("zeNFT", function () {
 
   it("NFTs have correct metadata after mint", async function () {
     const NFT = await ethers.getContractFactory("zeNFT");
-    const nft = await NFT.deploy("ZEN", "ZEN", "zenft.farm/");
+    const nft = await NFT.deploy(name, symbol, baseURI, mintCap, 0);
     await nft.deployed();
 
-    await nft.mint();
-    expect(await nft.tokenURI(0)).to.equal("zenft.farm/0");
     await nft.mint();
     expect(await nft.tokenURI(1)).to.equal("zenft.farm/1");
     await nft.mint();
     expect(await nft.tokenURI(2)).to.equal("zenft.farm/2");
+    await nft.mint();
+    expect(await nft.tokenURI(3)).to.equal("zenft.farm/3");
   });
 
   it("NFT minters have correct balances and total supply matches", async function () {
     [minter0, minter1] = await ethers.getSigners();
 
     const NFT = await ethers.getContractFactory("zeNFT");
-    const nft = await NFT.deploy("ZEN", "ZEN", "zenft.farm/");
+    const nft = await NFT.deploy(name, symbol, baseURI, mintCap, 0);
     await nft.deployed();
 
     await nft.mint();
@@ -56,42 +58,113 @@ describe("zeNFT", function () {
     [burner0, burner1] = await ethers.getSigners();
 
     const NFT = await ethers.getContractFactory("zeNFT");
-    const nft = await NFT.deploy("ZEN", "ZEN", "zenft.farm/");
+    const nft = await NFT.deploy(name, symbol, baseURI, mintCap, 0);
     await nft.deployed();
 
     await nft.mint();
     expect(await nft.balanceOf(burner0.address)).to.equal("1");
-    await nft.burn(0);
+    await nft.burn(1);
     expect(await nft.balanceOf(burner0.address)).to.equal("0");
     await nft.mint();
     expect(await nft.balanceOf(burner0.address)).to.equal("1");
 
     await nft.connect(burner1).mint();
     expect(await nft.balanceOf(burner1.address)).to.equal("1");
-    await nft.connect(burner1).burn(2);
+    await nft.connect(burner1).burn(3);
     expect(await nft.balanceOf(burner1.address)).to.equal("0");
 
     expect(await nft.totalSupply()).to.equal("1");
   });
 
-  it("NFT holders can transfer tokens among EOAs, have correct balances, and total supply doesn't change", async function () {
+  it("NFT holders can transfer tokens, have correct balances, and total supply doesn't change", async function () {
     [holder0, holder1, holder2] = await ethers.getSigners();
 
     const NFT = await ethers.getContractFactory("zeNFT");
-    const nft = await NFT.deploy("ZEN", "ZEN", "zenft.farm/");
+    const nft = await NFT.deploy(name, symbol, baseURI, mintCap, 0);
     await nft.deployed();
 
     await nft.mint();
-    await nft.transferFrom(holder0.address, holder1.address, 0);
+    await nft.transferFrom(holder0.address, holder1.address, 1);
     expect(await nft.balanceOf(holder0.address)).to.equal("0");
     expect(await nft.balanceOf(holder1.address)).to.equal("1");
 
     await nft.connect(holder2).mint();
     expect(await nft.balanceOf(holder2.address)).to.equal("1");
-    await nft.connect(holder2).transferFrom(holder2.address, holder1.address, 1);
+    await nft.connect(holder2).transferFrom(holder2.address, holder1.address, 2);
     expect(await nft.balanceOf(holder2.address)).to.equal("0");
     expect(await nft.balanceOf(holder1.address)).to.equal("2");
 
     expect(await nft.totalSupply()).to.equal("2");
+  });
+
+  it("NFT minting with correct fee works", async function () {
+    [holder0] = await ethers.getSigners();
+
+    const NFT = await ethers.getContractFactory("zeNFT");
+    const nft = await NFT.deploy(name, symbol, baseURI, mintCap, mintFee);
+    await nft.deployed();
+
+    await nft.mint({ value: ethers.utils.parseEther("0.5") });
+  });
+
+  it("NFT minting without correct fee doesn't work", async function () {
+    [holder0] = await ethers.getSigners();
+
+    const NFT = await ethers.getContractFactory("zeNFT");
+    const nft = await NFT.deploy(name, symbol, baseURI, mintCap, mintFee);
+    await nft.deployed();
+
+    await nft.mint().should.be.revertedWith("ETH_FEE_NOT_ATTACHED");
+  });
+
+  it("NFT minting works up to cap", async function () {
+    [holder0] = await ethers.getSigners();
+
+    const NFT = await ethers.getContractFactory("zeNFT");
+    const nft = await NFT.deploy(name, symbol, baseURI, mintCap, mintFee);
+    await nft.deployed();
+
+    await nft.mint({ value: ethers.utils.parseEther("0.5") });
+    await nft.mint({ value: ethers.utils.parseEther("0.5") });
+    await nft.mint({ value: ethers.utils.parseEther("0.5") });
+    await nft.mint({ value: ethers.utils.parseEther("0.5") });
+    await nft.mint({ value: ethers.utils.parseEther("0.5") });
+  });
+
+  it("NFT minting does not work past cap", async function () {
+    [holder0] = await ethers.getSigners();
+
+    const NFT = await ethers.getContractFactory("zeNFT");
+    const nft = await NFT.deploy(name, symbol, baseURI, mintCap, mintFee);
+    await nft.deployed();
+
+    await nft.mint({ value: ethers.utils.parseEther("0.5") });
+    await nft.mint({ value: ethers.utils.parseEther("0.5") });
+    await nft.mint({ value: ethers.utils.parseEther("0.5") });
+    await nft.mint({ value: ethers.utils.parseEther("0.5") });
+    await nft.mint({ value: ethers.utils.parseEther("0.5") });
+    await nft.mint({ value: ethers.utils.parseEther("0.5") }).should.be.revertedWith("CAPPED");
+  });
+
+  it("NFT fees in ETH are collectable by owner", async function () {
+    [holder0, holder1] = await ethers.getSigners();
+
+    const NFT = await ethers.getContractFactory("zeNFT");
+    const nft = await NFT.deploy(name, symbol, baseURI, mintCap, mintFee);
+    await nft.deployed();
+
+    await nft.connect(holder1).mint({ value: mintFee });
+    await nft.collectETH();
+  });
+
+  it("NFT fees in ETH are not collectable by non-owner", async function () {
+    [holder0, holder1, holder2] = await ethers.getSigners();
+
+    const NFT = await ethers.getContractFactory("zeNFT");
+    const nft = await NFT.deploy(name, symbol, baseURI, mintCap, mintFee);
+    await nft.deployed();
+
+    await nft.connect(holder1).mint({ value: mintFee });
+    await nft.connect(holder2).collectETH().should.be.revertedWith("Ownable: caller is not the owner");
   });
 });
